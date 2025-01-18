d9d54fab5adf6805a0c5b7709d9d936412f94c00
Edmund Miller <edmund.miller@seqera.io>
2025-01-18 16:22:53 -0600

    build: Loosen dependencies for Altair 5

--- altair_upset/__init__.py
+++ altair_upset/__init__.py
@@ -3,5 +3,5 @@
 from .upset import UpSetAltair
 from .config import upsetaltair_top_level_configuration
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["UpSetAltair", "upsetaltair_top_level_configuration"]
