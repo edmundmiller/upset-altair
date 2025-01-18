547092a627efc49719bce9b4c1909affacc72ac4
Edmund Miller <edmund.miller@seqera.io>
2025-01-18 12:42:17 -0600

    build: Make functions available

--- altair_upset/__init__.py
+++ altair_upset/__init__.py
@@ -1,5 +1,5 @@
 """UpSet plots using Altair."""
-from .original_function import UpSetAltair
+from .original_function import UpSetAltair, upsetaltair_top_level_configuration
 
 __version__ = "0.1.0"
-__all__ = ["UpSetAltair"]
+__all__ = ["UpSetAltair", "upsetaltair_top_level_configuration"]
