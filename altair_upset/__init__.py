ae328a1f2938e28fd8005a10c6a395cdf59bf080
Edmund Miller <git@edmundmiller.dev>
2025-01-18 12:28:16 -0600

    build: Copy over pyproject for pixi

---
+++ altair_upset/__init__.py
@@ -0,0 +1,7 @@
+"""UpSet plots using Altair."""
+from .original_function import UpSetAltair
+
+from .upset import UpSetAltair
+from .config import upsetaltair_top_level_configuration
+
+__all__ = ["UpSetAltair", "upsetaltair_top_level_configuration"]
